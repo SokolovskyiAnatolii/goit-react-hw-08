8a2bf89106de310b7ea69f96f50ca9a217fe0acf
Sokolovskyi Anatolii <42448977+SokolovskyiAnatolii@users.noreply.github.com>
2025-02-14 15:53:07 +0200

    Initial commit

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './contactsOps';
+import customToast from '../components/Toast/Toast';
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+  customToast('error', 'Oops... something went wrong, try again!');
+};
+
+const handleFulfilled = state => {
+  state.isLoading = false;
+  state.error = null;
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        handleFulfilled(state);
+        state.items = action.payload;
+        customToast('success', 'Countacts are loaded');
+      })
+      .addCase(fetchContacts.rejected, handleRejected)
+      .addCase(addContact.pending, handlePending)
+      .addCase(addContact.fulfilled, (state, action) => {
+        handleFulfilled(state);
+        state.items.push(action.payload);
+        customToast('success', `Contact added: ${action.payload.name}`);
+      })
+      .addCase(addContact.rejected, handleRejected)
+      .addCase(deleteContact.pending, handlePending)
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        handleFulfilled(state);
+        state.items = state.items.filter(item => item.id !== action.payload);
+        customToast('success', 'Contact removed');
+      })
+      .addCase(deleteContact.rejected, handleRejected);
+  },
+});
+
+export const contactsReducer = contactsSlice.reducer;
